3dd71e5e29d3060fbcadb90944e6c8db8c810ef6
jhansi-19 <jhansi.g23@iiits.in>
2025-10-15 21:04:14 +0530

    Update aws-config.js

--- src/aws-config.js
+++ src/aws-config.js
@@ -5,18 +5,25 @@ const awsmobile = {
   aws_content_delivery_bucket: 'ccfrontend-20251014194826-hostingbucket-dev',
   aws_content_delivery_bucket_region: 'us-east-1',
   aws_content_delivery_url: 'https://d3qc56qfcpana.cloudfront.net',
+  
   Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_DLkiT6Y7r',
-    userPoolWebClientId: '2o27e8efr1t6hmm2gsc3l6aol6',
-    oauth: {
-      domain: 'us-east-1dlkit6y7r.auth.us-east-1.amazoncognito.com', // e.g., quiz-app-pool.auth.us-east-1.amazoncognito.com
-      scope: ['email', 'openid', 'profile'],
-      redirectSignIn: 'https://dev.dk984uju67rhc.amplifyapp.com/',
-      redirectSignOut: 'https://dev.dk984uju67rhc.amplifyapp.com/',
-      responseType: 'code'
+    Cognito: {
+      userPoolId: 'us-east-1_DLkiT6Y7r',
+      userPoolClientId: '2o27e8efr1t6hmm2gsc3l6aol6',
+      signUpVerificationMethod: 'code',
+      loginWith: {
+        email: true,
+        oauth: {
+          domain: 'us-east-1dlkit6y7r.auth.us-east-1.amazoncognito.com',
+          scopes: ['email', 'openid', 'profile'],
+          redirectSignIn: ['https://dev.dk984uju67rhc.amplifyapp.com/'],
+          redirectSignOut: ['https://dev.dk984uju67rhc.amplifyapp.com/'],
+          responseType: 'code'
+        }
+      }
     }
   },
+  
   API: {
     REST: {
       quizApi: {
