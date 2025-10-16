169dbca6af893dd1acc2458a59ab4936d1a6a4af
jhansi-19 <jhansi.g23@iiits.in>
2025-10-16 08:23:08 +0530

    Refactor AWS configuration for Amplify setup

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,19 +1,11 @@
-import { Amplify } from 'aws-amplify';
-
-const awsmobile = {
-  aws_project_region: 'us-east-1',
-  aws_content_delivery_bucket: 'ccfrontend-20251014194826-hostingbucket-dev',
-  aws_content_delivery_bucket_region: 'us-east-1',
-  aws_content_delivery_url: 'https://d3qc56qfcpana.cloudfront.net',
-  
+const awsconfig = {
   Auth: {
     Cognito: {
       userPoolId: 'us-east-1_DLkiT6Y7r',
       userPoolClientId: '2o27e8efr1t6hmm2gsc3l6aol6',
       signUpVerificationMethod: 'code',
       loginWith: {
-        email: true,
-        username: false
+        email: true
       }
     }
   },
@@ -28,6 +20,4 @@ const awsmobile = {
   }
 };
 
-Amplify.configure(awsmobile);
-
-export default awsmobile;
+export default awsconfig;
