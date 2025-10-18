0dc31a1c2eea927d8dd9e01bf3a951e6b0bc2bd7
jhansi-19 <jhansi.g23@iiits.in>
2025-10-19 01:04:27 +0530

    Update aws-config.js

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,23 +1,28 @@
+import { Amplify } from 'aws-amplify';
+
+const isSecondaryRegion = window.location.hostname.includes('dev.<dr-app-id>.amplifyapp.com');
+
 const awsconfig = {
   Auth: {
     Cognito: {
-      userPoolId: 'us-east-1_DLkiT6Y7r',
-      userPoolClientId: '2o27e8efr1t6hmm2gsc3l6aol6',
+      region: isSecondaryRegion ? 'us-west-2' : 'us-east-1',
+      userPoolId: isSecondaryRegion ? 'us-west-2_pl4W32FsW' : 'us-east-1_DLkiT6Y7r',
+      userPoolClientId: isSecondaryRegion ? '7hrdm80pqb323iqo1tspi9dmin' : '2o27e8efr1t6hmm2gsc3l6aol6',
       signUpVerificationMethod: 'code',
       loginWith: {
         email: true
       }
     }
   },
-  
   API: {
     REST: {
       quizApi: {
-        endpoint: 'https://poavf33mhl.execute-api.us-east-1.amazonaws.com/prod',
-        region: 'us-east-1'
+        endpoint: 'https://api.quizapp.com/prod',
+        region: isSecondaryRegion ? 'us-west-2' : 'us-east-1'
       }
     }
   }
 };
 
+Amplify.configure(awsconfig);
 export default awsconfig;
